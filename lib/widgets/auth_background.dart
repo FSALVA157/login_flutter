f1425f975c5ab1b725fd96251963d69f6073a42c
Fernando Salva <xxavierargentino@hotmail.com>
2021-12-03 18:52:52 -0300

    first commit

---
+++ lib/widgets/auth_background.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class AuthBackground extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final anchoTotal = MediaQuery.of(context).size.width;
+    final altoTotal = MediaQuery.of(context).size.height;
+    return Container(
+      width: anchoTotal,
+      height: altoTotal,
+      color: Colors.grey,
+      child: Stack(
+        children: [
+          _PurpleBox(
+            ancho: anchoTotal,
+            alto: altoTotal,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _PurpleBox extends StatelessWidget {
+  final double ancho;
+  final double alto;
+
+  const _PurpleBox({Key? key, required this.ancho, required this.alto})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: ancho,
+      height: alto * 0.4,
+      decoration: _purpleBackground(),
+      child: Stack(
+        children: [
+          Positioned(top: 90, left: 30, child: _Bubbles()),
+          Positioned(top: -40, left: -30, child: _Bubbles()),
+          Positioned(top: -50, right: -20, child: _Bubbles()),
+          Positioned(bottom: -50, left: 10, child: _Bubbles()),
+          Positioned(bottom: 120, right: 20, child: _Bubbles())
+        ],
+      ),
+    );
+  }
+
+  BoxDecoration _purpleBackground() => BoxDecoration(
+          gradient: LinearGradient(colors: [
+        Color.fromRGBO(63, 63, 100, 1),
+        Color.fromRGBO(90, 70, 178, 1)
+      ]));
+}
+
+class _Bubbles extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          color: Color.fromRGBO(255, 255, 255, 0.06)),
+    );
+  }
+}
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,1 @@
+export 'package:flutter_productos/widgets/auth_background.dart';
